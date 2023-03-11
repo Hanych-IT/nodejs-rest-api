32394cf58b3fa10e7e88d19f4536f615220b2216
Dmytro Hanych <Hanych_IT@ukr.net>
2023-03-11 15:45:05 +0200

    corrected e-mail

--- controllers/tests/login.test.js
+++ controllers/tests/login.test.js
@@ -1,42 +1,42 @@
 describe("Login controller test", () => {
   it("login with email and password", async () => {
     jest.mock("../auth/login.js", async () => {
-      const controller = jest.requireActual("../auth/login.js");
-      jest.spyOn(controller, "login");
+      const controller = jest.requireActual("../auth/login.js")
+      jest.spyOn(controller, "login")
 
-      const req = { body: { email: "medoweek@ukr.net", password: "1111" } };
-      const res = {};
+      const req = { body: { email: "medoweek@ukr.net", password: "1111" } }
+      const res = {}
 
-      const result = await controller(req, res);
+      const result = await controller(req, res)
 
-      expect(result.statusCode).toBe(200);
-      expect(result.token).toBeTruthy();
-      expect(result.email).toBeTruthy();
-      expect(result.subscription).toBeTruthy();
+      expect(result.statusCode).toBe(200)
+      expect(result.token).toBeTruthy()
+      expect(result.email).toBeTruthy()
+      expect(result.subscription).toBeTruthy()
       expect(result).toEqual({
-        email: "medoweek@ukr.nets",
+        email: "medoweek@ukr.net",
         subscription: "starter",
-      });
-      expect(result.email).toMatch(/\w/);
-      expect(result.subscription).toMatch(/\w/);
-    });
-  });
-});
+      })
+      expect(result.email).toMatch(/\w/)
+      expect(result.subscription).toMatch(/\w/)
+    })
+  })
+})
 
-const { User } = require('../models')
-const auth = require('../middlewares/auth')
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const { User } = require("../models")
+const auth = require("../middlewares/auth")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 const { SECRET_KEY } = process.env
 
-describe('Auth middleware test', () => {
-  it('should called next() and added user property to req object', async () => {
-    const mockUserId = '123456'
+describe("Auth middleware test", () => {
+  it("should called next() and added user property to req object", async () => {
+    const mockUserId = "123456"
     const token = jwt.sign({ _id: mockUserId }, SECRET_KEY)
     const user = {
       _id: mockUserId,
-      email: 'vasylbarna@gmail.com',
+      email: "vasylbarna@gmail.com",
       token,
     }
 
@@ -48,7 +48,7 @@ describe('Auth middleware test', () => {
     const mRes = {}
     const mockNext = jest.fn()
 
-    jest.spyOn(User, 'findById').mockImplementationOnce(async () => user)
+    jest.spyOn(User, "findById").mockImplementationOnce(async () => user)
 
     await auth(mReq, mRes, mockNext)
 
